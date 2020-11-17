fd92f45f7549d4b346af8d6acf404d4c27d57ccd
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-17 15:19:53 +0100

    Style home-banner

--- template-parts/home-banner.php
+++ template-parts/home-banner.php
@@ -1,5 +1,14 @@
-<div class="banner">
-    <h1 class="banner__motto">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/images/fastestin.webp" alt="Najszybsi w Internecie" class="banner__image">
-    </h1>
+<div class="home-banner">
+    <div class="home-banner__container">
+        <h1 class="home-banner__motto">
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/fastestin.webp" alt="Najszybsi w Internecie" class="home-banner__image">
+        </h1>
+        <p class="services">
+            <a href="#" class="services__item" rel="noopener noreferrer">Strony www</a>
+            <span class="services__separator">|</span>
+            <a href="#" class="services__item" rel="noopener noreferrer">Sklepy internetowe</a>
+            <span class="services__separator">|</span>
+            <a href="#" class="services__item" rel="noopener noreferrer">Marketing internetowy</a>
+        </p>
+    </div>
 </div>
