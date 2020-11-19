1c11fce649824d3d6beab8823559ccfba79f0cc5
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 16:53:26 +0100

    Add page-zdrojowisko with zdrojowisko-page-header

---
+++ template-parts/zdrojowisko-page-header.php
@@ -0,0 +1,5 @@
+<div class="page-banner-no-img zdrojowisko-page-header">
+    <header class="page-banner-no-img__header">
+        <h1 class="page-banner-no-img__title">Zdrojowisko</h1>
+    </header>
+</div>
