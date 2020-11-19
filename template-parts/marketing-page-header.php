fc2fdd8f5ff80e998e8688d6f9aec0111e1e878a
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 16:25:01 +0100

    Add page-marketing-internetowy with marketing-page-header

---
+++ template-parts/marketing-page-header.php
@@ -0,0 +1,12 @@
+<div class="page-banner-motto marketing-page-header">
+    <div class="page-banner-motto__img-container">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/panther-purple.webp" alt="" class="page-banner-motto__img">
+        <p class="page-banner-motto__title">Branding</p>
+    </div>
+    <div class="page-banner-motto__motto-container">
+        <h1>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/fastestin-branding.webp" alt="Strony www" class="page-banner-motto__motto">
+        </h1>
+        <p class="page-banner-motto__description">Stworzymy wizerunek Twojej firmy, jako marki wyjątkowej, nowoczesnej i łatwo rozpoznawalnej.</p>
+    </div>
+</div>
