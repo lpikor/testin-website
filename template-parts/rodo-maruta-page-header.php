b98c7e5843facc75b65c0d9522e69e68f37e666d
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 17:02:46 +0100

    Add page-rodo-maruta with rodo-maruta-page-header

---
+++ template-parts/rodo-maruta-page-header.php
@@ -0,0 +1,5 @@
+<div class="page-banner-no-img rodo-maruta-page-header">
+    <header class="page-banner-no-img__header">
+        <h1 class="page-banner-no-img__title">Rodo Maruta</h1>
+    </header>
+</div>
