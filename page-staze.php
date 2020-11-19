aeded60ec9cf5b77152a5c8fcccea163be420e7a
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 17:30:36 +0100

    Add page-staze with internships-page-header

---
+++ page-staze.php
@@ -0,0 +1,5 @@
+<?php get_header();
+
+    get_template_part('template-parts/internships-page-header');
+
+get_footer(); ?>
---
+++ template-parts/internships-page-header.php
@@ -0,0 +1,10 @@
+<div class="page-banner-img internships-page-header">
+    <div class="page-banner-img__img-container">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/recruiter-circles.webp" alt="" class="page-banner-img__img">
+    </div>
+    <header class="page-banner-img__header">
+        <h1 class="page-banner-img__title">Staże</h1>
+        <p class="page-banner-img__description">Testin to firma, w której podniesiesz swoje kwalifikacje.</p>
+        <p class="page-banner-img__description">Jeśli nie boisz się podejmowania wyzwań i jesteś osobą otwartą na zdobywanie nowych umiejętności – spróbuj swoich sił w naszej firmie!</p>
+    </header>
+</div>
