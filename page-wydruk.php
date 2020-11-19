7870823c74cad2fb2f4ea0bd9d9d17b1e2686888
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 16:34:23 +0100

    Add page-wydruk with print-page-header

---
+++ page-wydruk.php
@@ -0,0 +1,5 @@
+<?php get_header();
+
+    get_template_part('template-parts/print-page-header');
+
+get_footer(); ?>
---
+++ template-parts/print-page-header.php
@@ -0,0 +1,12 @@
+<div class="page-banner-motto print-page-header">
+    <div class="page-banner-motto__img-container">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/panther-pallette-red.webp" alt="" class="page-banner-motto__img">
+        <p class="page-banner-motto__title">Wydruk</p>
+    </div>
+    <div class="page-banner-motto__motto-container">
+        <h1>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/fastestin-printing.webp" alt="Wydruk" class="page-banner-motto__motto">
+        </h1>
+        <p class="page-banner-motto__description">Twojego logo i produkty muszą być rozpoznawalne. Zadbaj o odpowiednią kreację graficzną i przygotowanie materiałów, które wykorzystasz do promocji i sprzedaży usług lub produktów.</p>
+    </div>
+</div>
