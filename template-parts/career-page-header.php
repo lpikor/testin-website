1ee13c3c07c2df6ac8b3a37b4cf34c2487e204d8
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 17:23:18 +0100

    Add page-kariera with career-page-header

---
+++ template-parts/career-page-header.php
@@ -0,0 +1,9 @@
+<div class="page-banner-img career-page-header">
+    <div class="page-banner-img__img-container">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/brainstorming.webp" alt="" class="page-banner-img__img">
+    </div>
+    <header class="page-banner-img__header">
+        <h1 class="page-banner-img__title">Dołącz do naszego zespołu!</h1>
+        <p class="page-banner-img__description">Szukasz pracy, do której każdego dnia będziesz przychodził z uśmiechem na twarzy? Gdzie podniesiesz swoje kwalifikacje, nawiążesz nowe, ciekawe znajomości i wykorzystasz potencjał, który w Tobie drzemie?</p>
+    </header>
+</div>
