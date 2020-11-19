db26e3367b39a754454086564aa87f758705832a
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 15:32:15 +0100

    Style page-banner templates

---
+++ template-parts/page-headers.php
@@ -0,0 +1,29 @@
+<!-- <div class="page-banner-no-img">
+    <header class="page-banner-no-img__header">
+        <h1 class="page-banner-no-img__title">Nasza oferta</h1>
+        <p class="page-banner-no-img__description">Powiedz nam, jaki cel chcesz osiągnąć, a my zajmiemy się całą resztą. Przekonaj się, dlaczego warto z nami współpracować!</p>
+    </header>
+</div> -->
+
+<!-- <div class="page-banner-motto">
+    <div class="page-banner-motto__img-container">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/panther-laying.webp" alt="" class="page-banner-motto__img">
+        <p class="page-banner-motto__title">Strony www</p>
+    </div>
+    <div class="page-banner-motto__motto-container">
+        <h1>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/fastestin-websites.webp" alt="Strony www" class="page-banner-motto__motto">
+        </h1>
+        <p class="page-banner-motto__description">Twoja wizytówka w sieci? Strona internetowa! Stworzymy dla Ciebie intuicyjną, przejrzystą i przykuwającą uwagę stronę WWW, która pomoże Ci zaistnieć w sieci i zdobyć nowych klientów.</p>
+    </div>
+</div> -->
+
+<!-- <div class="page-banner-img">
+    <div class="page-banner-img__img-container">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/puzzle-circles.webp" alt="" class="page-banner-img__img">
+    </div>
+    <header class="page-banner-img__header">
+        <h1 class="page-banner-img__title">Nasze realizacje</h1>
+        <p class="page-banner-img__description">Różne branże, różne lokalizacje, różne grupy docelowe, ale jeden cel - widoczność firmy w sieci. Projektując i wdrażając strony i sklepy internetowe, zwracamy uwagę na każdy szczegół. Przykuwająca uwagę szata graficzna, zrozumiały tekst, przejrzystość i intuicyjność - oto kilka cech naszych realizacji.</p>
+    </header>
+</div> -->
