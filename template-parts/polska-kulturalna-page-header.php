0b04c7be50889ac422fa6d0b351559d4820d1326
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 16:41:58 +0100

    Add polska-kulturalna-page with polska-kulturalna-page-header

---
+++ template-parts/polska-kulturalna-page-header.php
@@ -0,0 +1,5 @@
+<div class="page-banner-no-img polska-kulturalna-page-header">
+    <header class="page-banner-no-img__header">
+        <h1 class="page-banner-no-img__title">Polska Kulturalna</h1>
+    </header>
+</div>
