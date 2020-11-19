6108e3793b4a6fd50368c63357379e50ca79e764
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 17:18:08 +0100

    Add single post page with single-post-page-header

---
+++ single.php
@@ -0,0 +1,5 @@
+<?php get_header();
+
+    get_template_part('template-parts/single-post-page-header');
+
+get_footer(); ?>
---
+++ template-parts/single-post-page-header.php
@@ -0,0 +1,5 @@
+<div class="page-banner-no-img single-post-page-header">
+    <header class="page-banner-no-img__header">
+        <h1 class="page-banner-no-img__title">Nasza oferta</h1>
+    </header>
+</div>
