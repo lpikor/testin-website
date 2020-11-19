f065a61892ad6dc97793645c2a71ac3439bb9233
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 17:04:43 +0100

    Add page-gamrat-wpc with gamrat-wpc-page-header

---
+++ page-gamrat-wpc.php
@@ -0,0 +1,5 @@
+<?php get_header();
+
+    get_template_part('template-parts/gamrat-wpc-page-header');
+
+get_footer(); ?>
---
+++ template-parts/gamrat-wpc-page-header.php
@@ -0,0 +1,5 @@
+<div class="page-banner-no-img gamrat-wpc-page-header">
+    <header class="page-banner-no-img__header">
+        <h1 class="page-banner-no-img__title">Gamrat WPC</h1>
+    </header>
+</div>
