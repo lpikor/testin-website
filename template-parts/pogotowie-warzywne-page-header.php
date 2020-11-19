f26fdc8858785cc566de5c8c9e5e14abd5050b7a
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 16:51:08 +0100

    Add page-pogotowie-warzywne with pogotowie-warzywne-page-header

---
+++ template-parts/pogotowie-warzywne-page-header.php
@@ -0,0 +1,5 @@
+<div class="page-banner-no-img pogotowie-warzywne-page-header">
+    <header class="page-banner-no-img__header">
+        <h1 class="page-banner-no-img__title">Pogotowie Warzywne</h1>
+    </header>
+</div>
