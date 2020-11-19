8a7c801a12ae8df196a2170ac1714c2f4d39078d
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 17:36:58 +0100

    Add page-staz with internship-page-header for single internship

---
+++ template-parts/internship-page-header.php
@@ -0,0 +1,9 @@
+<div class="page-banner-img internship-page-header">
+    <div class="page-banner-img__img-container">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/info-book-circles.png" alt="" class="page-banner-img__img">
+    </div>
+    <header class="page-banner-img__header">
+        <h1 class="page-banner-img__title">Anglista staż Rzeszów</h1>
+        <p class="page-banner-img__description">Testin to firma zajmująca się kompleksową reklamą firm w Internecie, tworzeniem profesjonalnych stron internetowych, rozbudowanych serwisów i sklepów. Robimy wszystko, by nasz klient mógł odnieść sukces.</p>
+    </header>
+</div>
