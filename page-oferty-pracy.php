31b254ed0fed8863bdd72dbb17f07def1ecd2daa
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 17:27:12 +0100

    Add page-oferty-pracy with job-offers-page-header

---
+++ page-oferty-pracy.php
@@ -0,0 +1,5 @@
+<?php get_header();
+
+    get_template_part('template-parts/job-offers-page-header');
+
+get_footer(); ?>
---
+++ template-parts/job-offers-page-header.php
@@ -0,0 +1,10 @@
+<div class="page-banner-img job-offers-page-header">
+    <div class="page-banner-img__img-container">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/applications-circles.webp" alt="" class="page-banner-img__img">
+    </div>
+    <header class="page-banner-img__header">
+        <h1 class="page-banner-img__title">Aktualne oferty pracy</h1>
+        <p class="page-banner-img__description">Jesteśmy doświadczoną w swojej branży firmą, która posiada ugruntowaną pozycję na rynku. Wciąż się rozwijając, z roku na rok poszerzamy grono stałych klientów, którzy  - doceniając naszą rzetelność - polecają nasze usługi innym.</p>
+        <p class="page-banner-img__description">związku z dynamicznym wzrostem otrzymywanych zleceń, chętnie powiększymy nasz zespół o nowych specjalistów. Chciałbyś do nas dołączyć? Sprawdź kogo aktualnie potrzebujemy do naszej ekipy!</p>
+    </header>
+</div>
