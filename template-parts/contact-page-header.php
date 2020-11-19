c0773b22a643094627ef84e03d175c8920ccb2af
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-19 17:32:59 +0100

    Add page-kontakt with contact-page-header

---
+++ template-parts/contact-page-header.php
@@ -0,0 +1,6 @@
+<div class="page-banner-no-img contact-page-header">
+    <header class="page-banner-no-img__header">
+        <h1 class="page-banner-no-img__title">Kontakt</h1>
+        <p class="page-banner-no-img__description">Napisz, czego potrzebujesz, a my skontaktujemy się z Tobą. Strona WWW, sklep internetowy, promocja w sieci - jesteśmy do Twojej dyspozycji.</p>
+    </header>
+</div>
