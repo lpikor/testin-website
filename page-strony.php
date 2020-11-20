24598f92bc1c3e260a9b42bde5b6fb99b0513f5d
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-20 08:12:51 +0100

    Style offer-child-contact-us and add webistes-contact-us to page-oferta

--- page-strony.php
+++ page-strony.php
@@ -2,4 +2,6 @@
 
     get_template_part('template-parts/websites-page-header');
 
+    get_template_part('template-parts/websites-contact-us');
+
 get_footer(); ?>
---
+++ template-parts/websites-contact-us.php
@@ -0,0 +1,9 @@
+<section class="offer-child-contact-us websites-contact-us">
+    <div class="offer-child-contact-us__container">
+        <header class="offer-child-contact-us__header">
+            <h2 class="offer-child-contact-us__title">Lorem ipsum dolor sit?</h2>
+            <p class="offer-child-contact-us__description">Nasi specjaliści przygotują dla Ciebie nowoczesną i funkcjonalną stronę internetową. Zadbaj o pozytywny wizerunek firmy i zwiększ zasięg swojej oferty!</p>
+        </header>
+        <a href="<?php echo get_permalink(9602); ?>" class="offer-child-contact-us__btn btn">Napisz do nas</a>
+    </div>
+</section>
