af90377499ba45d8703532ed134f6a75ac42061f
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-20 06:49:03 +0100

    Add page-strony with websites-page-header

---
+++ template-parts/websites-page-header.php
@@ -0,0 +1,12 @@
+<div class="page-banner-motto websites-page-header">
+    <div class="page-banner-motto__img-container">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/panther-laying.webp" alt="" class="page-banner-motto__img">
+        <p class="page-banner-motto__title">Strony www</p>
+    </div>
+    <div class="page-banner-motto__motto-container">
+        <h1>
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/fastestin-websites.webp" alt="Strony www" class="page-banner-motto__motto">
+        </h1>
+        <p class="page-banner-motto__description">Twoja wizytówka w sieci? Strona internetowa! Stworzymy dla Ciebie intuicyjną, przejrzystą i przykuwającą uwagę stronę WWW, która pomoże Ci zaistnieć w sieci i zdobyć nowych klientów.</p>
+    </div>
+</div>
