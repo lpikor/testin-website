3a5899eb2c4c2bd9df335c4c5e4adfa4ddd6a031
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-25 12:52:02 +0100

    Style shops-contact-us

--- page-sklepy-internetowe.php
+++ page-sklepy-internetowe.php
@@ -2,4 +2,24 @@
 
     get_template_part('template-parts/shops-page-header');
 
+    get_template_part('template-parts/shops-contact-us');
+
+    get_template_part('template-parts/websites-advantages');
+
+    get_template_part('template-parts/websites-opportunities');
+
+    get_template_part('template-parts/our-projects');
+
+    get_template_part('template-parts/websites-encouragement');
+
+    get_template_part('template-parts/websites-form');
+
+    get_template_part('template-parts/websites-how-steps');
+
+    get_template_part('template-parts/websites-why-trust-us');
+
+    get_template_part('template-parts/websites-your-best');
+
+    get_template_part('template-parts/websites-form-2');
+
 get_footer(); ?>
---
+++ template-parts/shops-contact-us.php
@@ -0,0 +1,9 @@
+<section class="offer-child-contact-us shops-contact-us">
+    <div class="offer-child-contact-us__container">
+        <header class="offer-child-contact-us__header">
+            <h2 class="offer-child-contact-us__title">Chcesz zacząć sprzedawać przez internet?</h2>
+            <p class="offer-child-contact-us__description">Nasi specjaliści przygotują dla Ciebie nowoczesny i funkcjonalny sklep internetowy. Dzięki niemu pozyskasz nowych klientów, którzy znajdą Cię w Internecie!</p>
+        </header>
+        <a href="<?php echo get_permalink(9602); ?>" class="offer-child-contact-us__btn btn">Napisz do nas</a>
+    </div>
+</section>
