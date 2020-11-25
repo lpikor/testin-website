868f2adeded955461d621cba086caabd9f25ffc8
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-25 12:58:42 +0100

    Style marketing-contact-us

--- page-marketing-internetowy.php
+++ page-marketing-internetowy.php
@@ -2,4 +2,6 @@
 
     get_template_part('template-parts/marketing-page-header');
 
+    get_template_part('template-parts/marketing-contact-us');
+
 get_footer(); ?>
---
+++ template-parts/marketing-contact-us.php
@@ -0,0 +1,9 @@
+<section class="offer-child-contact-us marketing-contact-us">
+    <div class="offer-child-contact-us__container">
+        <header class="offer-child-contact-us__header">
+            <h2 class="offer-child-contact-us__title">Szukasz najlepszego sposobu na promocję?</h2>
+            <p class="offer-child-contact-us__description">Nasi specjaliści poprowadzą dla Ciebie blog firmowy oraz profile w mediach społecznościowych. Dzięki temu klienci szybko znajdą Cię w sieci i poznają Twoją ofertę.</p>
+        </header>
+        <a href="<?php echo get_permalink(9602); ?>" class="offer-child-contact-us__btn btn">Napisz do nas</a>
+    </div>
+</section>
