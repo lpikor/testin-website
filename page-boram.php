c4c5b5211e82ddc6c152048dc59eb78c3fb3a667
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-30 15:36:19 +0100

    Style boram-content

--- page-boram.php
+++ page-boram.php
@@ -2,4 +2,8 @@
 
     get_template_part('template-parts/boram-page-header');
 
+    get_template_part('template-parts/boram-content');
+
+    get_template_part('template-parts/our-projects-child-footer');
+
 get_footer(); ?>
---
+++ template-parts/boram-content.php
@@ -0,0 +1,31 @@
+<div class="our-projects-child-content boram-content">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/boram-desktop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop">
+    <div class="boram-content__red-bg"></div>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/boram-mobile.webp" alt="" class="our-projects-child-content__item our-projects-child-content__mobile">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/boram-tablet.webp" alt="" class="our-projects-child-content__item our-projects-child-content__tablet">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/boram-screen.webp" alt="" class="our-projects-child-content__item our-projects-child-content__screen">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/boram-desktop-2.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop-2">
+
+    <div class="our-projects-child-content__notes">
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">1</span>
+            <p class="our-projects-child-content__note-text">Klient zgłosił się do nas z potrzebą nowej strony www zintegrowanej ze sklepem online.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">2</span>
+            <p class="our-projects-child-content__note-text">Wykonaliśmy zlecenie z dedykowaną szatą graficzną - stronę w 3 wersjach językowych.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">3</span>
+            <p class="our-projects-child-content__note-text">Całość została spójnie przygotowana, z infografikami i wygodnym interfejsem.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">4</span>
+            <p class="our-projects-child-content__note-text">Strona została oczywiście dopasowana do urządzeń mobilnych.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">5</span>
+            <p class="our-projects-child-content__note-text">Wykreowaliśmy również wizerunek "Boba Budowniczego", który doradzał w wyborze najlepszych produktów Boramu.</p>
+        </div>
+    </div>
+</div>
