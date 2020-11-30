609b522b4a9ed615eea61e429fc3cf27e778cc93
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-30 14:57:17 +0100

    Style zdrojowisko-content

--- page-zdrojowisko.php
+++ page-zdrojowisko.php
@@ -2,4 +2,8 @@
 
     get_template_part('template-parts/zdrojowisko-page-header');
 
+    get_template_part('template-parts/zdrojowisko-content');
+
+    get_template_part('template-parts/our-projects-child-footer');
+
 get_footer(); ?>
---
+++ template-parts/zdrojowisko-content.php
@@ -0,0 +1,31 @@
+<div class="our-projects-child-content zdrojowisko-content">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/zdrojowisko-desktop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop">
+    <div class="zdrojowisko-content__blue-bg"></div>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/zdrojowisko-mobile.webp" alt="" class="our-projects-child-content__item our-projects-child-content__mobile">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/zdrojowisko-tablet.webp" alt="" class="our-projects-child-content__item our-projects-child-content__tablet">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/zdrojowisko-screen.webp" alt="" class="our-projects-child-content__item our-projects-child-content__screen">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/zdrojowisko-desktop-2.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop-2">
+
+    <div class="our-projects-child-content__notes">
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">1</span>
+            <p class="our-projects-child-content__note-text">Otrzymaliśmy zamówienie na serwis, który w przejrzysty sposób przedstawia ofertę sanatoriów i pozwala na rezerwację turnusu online.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">2</span>
+            <p class="our-projects-child-content__note-text">Powstało Zdrojowisko.pl - zaawansowany serwis rezerwacyjny, który działa na zasadzie Booking.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">3</span>
+            <p class="our-projects-child-content__note-text">Wykonaliśmy również newsletter, który zachęca do korzystania z systemu rezerwacji.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">4</span>
+            <p class="our-projects-child-content__note-text">Dzięki naszym usługom pozycjonowania strona znajduje się wysoko w Google, pozwalając znaleźć najlepsze uzdrowisko.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">5</span>
+            <p class="our-projects-child-content__note-text">Dzięki prowadzonej przez nas infolinii, wiele osób mogło zapytać o szczegóły i zarezerwować pobyt w najlepszym ośrodku.</p>
+        </div>
+    </div>
+</div>
