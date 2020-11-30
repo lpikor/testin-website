83f4c70c6d4990426626646db31a2b262f8dcbd6
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-30 10:45:33 +0100

    Style polska-kulturalna-content

--- page-polska-kulturalna.php
+++ page-polska-kulturalna.php
@@ -2,4 +2,6 @@
 
     get_template_part('template-parts/polska-kulturalna-page-header');
 
+    get_template_part('template-parts/polska-kulturalna-content');
+
 get_footer(); ?>
---
+++ template-parts/polska-kulturalna-content.php
@@ -0,0 +1,30 @@
+<div class="our-projects-child-content polska-kulturalna-content">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/polskakulturalna-desktop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/polskakulturalna-mobile.webp" alt="" class="our-projects-child-content__item our-projects-child-content__mobile">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/polskakulturalna-tablet.webp" alt="" class="our-projects-child-content__item our-projects-child-content__tablet">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/polskakulturalna-laptop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__laptop">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/polskakulturalna-screen.webp" alt="" class="our-projects-child-content__item our-projects-child-content__screen">
+    
+    <div class="our-projects-child-content__notes">
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">1</span>
+            <p class="our-projects-child-content__note-text">Twórcy projektu postanowili przypomnieć bohaterów polskiej popkultury umieszczając ich wizerunki na T-shirtach.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">2</span>
+            <p class="our-projects-child-content__note-text">Chcąc rozpropagować tę wyjątkową kolekcję, zwrócili się do nas z prośbą o przygotowanie dla Polski Kulturalnej sklepu internetowego.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">3</span>
+            <p class="our-projects-child-content__note-text">Stworzyliśmy rozbudowany, designerski e-sklep z intuicyjnym nawigowaniem i przejrzystym menu.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">4</span>
+            <p class="our-projects-child-content__note-text">Wszystkie podstrony sklepu zostały dopracowane w najdrobniejszych szczegółach, a sama strona dopasowana do urządzeń mobilnych.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">5</span>
+            <p class="our-projects-child-content__note-text">Polska Kulturalna zyskała szeroką publikę i wielu nowych klientów.</p>
+        </div>
+    </div>
+</div>
