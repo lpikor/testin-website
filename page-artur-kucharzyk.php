18f3aea2518891f54cba1fd6968df2ed556f0c93
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-30 11:37:21 +0100

    Style artur-kucharzyk-content

--- page-artur-kucharzyk.php
+++ page-artur-kucharzyk.php
@@ -2,4 +2,8 @@
 
     get_template_part('template-parts/artur-kucharzyk-page-header');
 
+    get_template_part('template-parts/artur-kucharzyk-content');
+
+    get_template_part('template-parts/our-projects-child-footer');
+
 get_footer(); ?>
---
+++ template-parts/artur-kucharzyk-content.php
@@ -0,0 +1,32 @@
+<div class="our-projects-child-content artur-kucharzyk-content">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/arturkucharzyk-desktop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop">
+    <div class="artur-kucharzyk-content__orange-bg"></div>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/arturkucharzyk-mobile.webp" alt="" class="our-projects-child-content__item our-projects-child-content__mobile">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/arturkucharzyk-tablet.webp" alt="" class="our-projects-child-content__item our-projects-child-content__tablet">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/arturkucharzyk-laptop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__laptop">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/arturkucharzyk-screen.webp" alt="" class="our-projects-child-content__item our-projects-child-content__screen">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/arturkucharzyk-desktop-2.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop-2">
+
+    <div class="our-projects-child-content__notes">
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">1</span>
+            <p class="our-projects-child-content__note-text">Klient zamówił u nas stronę www, która będzie promowała jego książkę i umożliwi zakup online.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">2</span>
+            <p class="our-projects-child-content__note-text">Stworzyliśmy stronę www ze sklepem, w którym znajduje się książka Artura Kucharzyka, a także inne produkty i usługi.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">3</span>
+            <p class="our-projects-child-content__note-text">Zostało przez nas wykonane również logo w kolorystyce związanej z dotychczasowa działalnością klienta.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">4</span>
+            <p class="our-projects-child-content__note-text">Cały sklep również został ujednolicony kolorystycznie, by zadbać o spójną identyfikację wizualną klienta.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">5</span>
+            <p class="our-projects-child-content__note-text">Obecnie zajmujemy się promocją książki oraz innych produktów i usług oferowanych przez klienta i jego firmę.</p>
+        </div>
+    </div>
+</div>
