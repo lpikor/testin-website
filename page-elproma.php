d516ac9f6a5769648dab330928367ee1c6d36a78
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-30 15:11:51 +0100

    Style elproma-content

--- page-elproma.php
+++ page-elproma.php
@@ -2,4 +2,8 @@
 
     get_template_part('template-parts/elproma-page-header');
 
+    get_template_part('template-parts/elproma-content');
+
+    get_template_part('template-parts/our-projects-child-footer');
+
 get_footer(); ?>
