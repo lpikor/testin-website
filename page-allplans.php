59f6ad3f555de31e3f2732c54d9288b36402b553
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-30 12:03:52 +0100

    Change our-projects-child layout to relative based instead of absolute

--- page-allplans.php
+++ page-allplans.php
@@ -2,4 +2,8 @@
 
     get_template_part('template-parts/allplans-page-header');
 
+    get_template_part('template-parts/allplans-content');
+
+    get_template_part('template-parts/our-projects-child-footer');
+
 get_footer(); ?>
---
+++ template-parts/allplans-content.php
@@ -0,0 +1,31 @@
+<div class="our-projects-child-content allplans-content">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/allplans-desktop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/allplans-mobile.webp" alt="" class="our-projects-child-content__item our-projects-child-content__mobile">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/allplans-tablet.webp" alt="" class="our-projects-child-content__item our-projects-child-content__tablet">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/allplans-laptop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__laptop">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/allplans-screen.webp" alt="" class="our-projects-child-content__item our-projects-child-content__screen">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/allplans-desktop-2.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop-2">
+
+    <div class="our-projects-child-content__notes">
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">1</span>
+            <p class="our-projects-child-content__note-text">Klient nie miał strony internetowej oraz chciał rozpocząć działania na terenie Rzeszowa i okolic.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">2</span>
+            <p class="our-projects-child-content__note-text">Dzięki zaangażowaniu naszego zespołu i owocnej współpracy z AllPlans powstało logo firmy oraz funkcjonalna strona www.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">3</span>
+            <p class="our-projects-child-content__note-text">Strona jest responsywna, dzięki czemu można ją przeglądać i wygodnie z niej korzystać na każdym urządzeniu.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">4</span>
+            <p class="our-projects-child-content__note-text">Znajduje się na niej zaawansowana wyszukiwarka projektów, w której można znaleźć z łatwością wymarzony dom.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">5</span>
+            <p class="our-projects-child-content__note-text">Skuteczne pozycjonowanie strony allplans.pl sprawiło, że znajduje się ona wysoko w wyszukiwarce Google.</p>
+        </div>
+    </div>
+</div>
