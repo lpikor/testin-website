3695694a628bb4c93f3c1096055cf3a6b9beb24b
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-30 15:50:15 +0100

    Style magnum-content

---
+++ template-parts/magnum-content.php
@@ -0,0 +1,30 @@
+<div class="our-projects-child-content magnum-content">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/magnum-desktop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop">
+    <div class="magnum-content__blue-bg"></div>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/magnum-mobile.webp" alt="" class="our-projects-child-content__item our-projects-child-content__mobile">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/magnum-tablet.webp" alt="" class="our-projects-child-content__item our-projects-child-content__tablet">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/magnum-screen.webp" alt="" class="our-projects-child-content__item our-projects-child-content__screen">
+
+    <div class="our-projects-child-content__notes">
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">1</span>
+            <p class="our-projects-child-content__note-text">Firma Magnum zwróciła się do nas z prośbą o wykonanie strony www, jak i odświeżenie wizerunku firmy.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">2</span>
+            <p class="our-projects-child-content__note-text">Stworzyliśmy stronę internetową, która w atrakcyjny sposób prezentuje produkty klienta.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">3</span>
+            <p class="our-projects-child-content__note-text">Wykonaliśmy również newsletter dla klienta, który pozwala na gromadzenie bazy mailingowej.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">4</span>
+            <p class="our-projects-child-content__note-text">Wykonaliśmy również newsletter dla klienta, który pozwala na gromadzenie bazy mailingowej.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">5</span>
+            <p class="our-projects-child-content__note-text">Strona jest dostosowana do urządzeń mobilnych, by można było wygodnie korzystać z jej funkcjonalności.</p>
+        </div>
+    </div>
+</div>
