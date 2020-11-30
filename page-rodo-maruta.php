c5b2e774e404b91294e63f9ba7295919a3dd6f0b
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-30 16:27:02 +0100

    Style rodo-maruta-content

--- page-rodo-maruta.php
+++ page-rodo-maruta.php
@@ -2,4 +2,8 @@
 
     get_template_part('template-parts/rodo-maruta-page-header');
 
+    get_template_part('template-parts/rodo-maruta-content');
+
+    get_template_part('template-parts/our-projects-child-footer');
+
 get_footer(); ?>
---
+++ template-parts/rodo-maruta-content.php
@@ -0,0 +1,32 @@
+<div class="our-projects-child-content rodo-maruta-content">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/maruta-desktop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop">
+    <div class="rodo-maruta-content__pink-bg"></div>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/maruta-mobile.webp" alt="" class="our-projects-child-content__item our-projects-child-content__mobile">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/maruta-tablet.webp" alt="" class="our-projects-child-content__item our-projects-child-content__tablet">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/maruta-screen.webp" alt="" class="our-projects-child-content__item our-projects-child-content__screen">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/maruta-desktop-2.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop-2">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/maruta-laptop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__laptop">
+
+    <div class="our-projects-child-content__notes">
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">1</span>
+            <p class="our-projects-child-content__note-text">Wykonaliśmy zaawansowany serwis internetowy, który pozwala na elastyczne wdrażanie zaleceń związanych z regulacjami RODO.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">2</span>
+            <p class="our-projects-child-content__note-text">Nowa strona została dopasowana do urządzeń mobilnych.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">3</span>
+            <p class="our-projects-child-content__note-text">Wzbogaciliśmy ją o newsletter oraz moduł blogowy przydatny do późniejszego pozycjonowania.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">4</span>
+            <p class="our-projects-child-content__note-text">Strona www posiada również zintegrowaną prezentację video oferty klienta.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">5</span>
+            <p class="our-projects-child-content__note-text">Nasz zespół zintegrował ją z mediami społecznościowymi.</p>
+        </div>
+    </div>
+</div>
