dac1f1abeb7e8bc387fe50e0e563c13ed04c3fa8
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-30 16:09:07 +0100

    Style vidok-content

--- page-vidok.php
+++ page-vidok.php
@@ -2,4 +2,8 @@
 
     get_template_part('template-parts/vidok-page-header');
 
+    get_template_part('template-parts/vidok-content');
+
+    get_template_part('template-parts/our-projects-child-footer');
+
 get_footer(); ?>
---
+++ template-parts/vidok-content.php
@@ -0,0 +1,31 @@
+<div class="our-projects-child-content vidok-content">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/vidok-desktop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop">
+    <div class="vidok-content__yellow-bg"></div>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/vidok-mobile.webp" alt="" class="our-projects-child-content__item our-projects-child-content__mobile">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/vidok-tablet.webp" alt="" class="our-projects-child-content__item our-projects-child-content__tablet">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/vidok-desktop-2.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop-2">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/vidok-screens.webp" alt="" class="our-projects-child-content__item our-projects-child-content__screens">
+
+    <div class="our-projects-child-content__notes">
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">1</span>
+            <p class="our-projects-child-content__note-text">Firma vidok zwróciła się do nas z prośbą o wykonanie strony www, jak i odświeżenie wizerunku firmy.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">2</span>
+            <p class="our-projects-child-content__note-text">Stworzyliśmy stronę internetową, która w atrakcyjny sposób prezentuje produkty klienta.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">3</span>
+            <p class="our-projects-child-content__note-text">Wykonaliśmy również newsletter dla klienta, który pozwala na gromadzenie bazy mailingowej.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">4</span>
+            <p class="our-projects-child-content__note-text">Wykonaliśmy również newsletter dla klienta, który pozwala na gromadzenie bazy mailingowej.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">5</span>
+            <p class="our-projects-child-content__note-text">Strona jest dostosowana do urządzeń mobilnych, by można było wygodnie korzystać z jej funkcjonalności.</p>
+        </div>
+    </div>
+</div>
