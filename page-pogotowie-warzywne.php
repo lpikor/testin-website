336c757d5feeb1998225375d3cc5c06e61eea628
Łukasz Pikor <pikorlukasz@gmail.com>
2020-11-30 14:24:18 +0100

    Style pogotowie-warzywne-content

--- page-pogotowie-warzywne.php
+++ page-pogotowie-warzywne.php
@@ -2,4 +2,8 @@
 
     get_template_part('template-parts/pogotowie-warzywne-page-header');
 
+    get_template_part('template-parts/pogotowie-warzywne-content');
+
+    get_template_part('template-parts/our-projects-child-footer');
+
 get_footer(); ?>
---
+++ template-parts/pogotowie-warzywne-content.php
@@ -0,0 +1,31 @@
+<div class="our-projects-child-content pogotowie-warzywne-content">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/pogotowiewarzywne-desktop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop">
+    <div class="pogotowie-warzywne-content__orange-bg"></div>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/pogotowiewarzywne-mobile.webp" alt="" class="our-projects-child-content__item our-projects-child-content__mobile">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/pogotowiewarzywne-tablet.webp" alt="" class="our-projects-child-content__item our-projects-child-content__tablet">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/pogotowiewarzywne-screen.webp" alt="" class="our-projects-child-content__item our-projects-child-content__screen">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/pogotowiewarzywne-desktop-2.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop-2">
+
+    <div class="our-projects-child-content__notes">
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">1</span>
+            <p class="our-projects-child-content__note-text">Stara strona klienta nie zawierała wykazu produktów, cen ani możliwości zamawiania online.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">2</span>
+            <p class="our-projects-child-content__note-text">Otrzymaliśmy zadanie wykonania nowej strony www w tydzień - sklepu online do wygodnego robienia zakupów.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">3</span>
+            <p class="our-projects-child-content__note-text">Daliśmy radę! Powstał sklep internetowy Pogotowie Warzywne, który pozwala na zakup świeżych owoców i warzyw.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">4</span>
+            <p class="our-projects-child-content__note-text">Posiada kalkulator, obliczający wartość przesyłki oraz umożliwia otrzymanie bezpłatnej dostawy.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">5</span>
+            <p class="our-projects-child-content__note-text">Naszego autorstwa jest również minimalistyczne logo klienta nawiązujące do jego działalności.</p>
+        </div>
+    </div>
+</div>
