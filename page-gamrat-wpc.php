5752aff8480dd7049d9031998b18fc3474957c5b
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-01 08:41:27 +0100

    Style gamrat-wpc-content

--- page-gamrat-wpc.php
+++ page-gamrat-wpc.php
@@ -2,4 +2,8 @@
 
     get_template_part('template-parts/gamrat-wpc-page-header');
 
+    get_template_part('template-parts/gamrat-wpc-content');
+
+    get_template_part('template-parts/our-projects-child-footer');
+
 get_footer(); ?>
---
+++ template-parts/gamrat-wpc-content.php
@@ -0,0 +1,30 @@
+<div class="our-projects-child-content gamrat-wpc-content">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/gamrat-desktop.webp" alt="" class="our-projects-child-content__item our-projects-child-content__desktop">
+    <div class="gamrat-wpc-content__blue-bg"></div>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/gamrat-mobile.webp" alt="" class="our-projects-child-content__item our-projects-child-content__mobile">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/gamrat-tablet.webp" alt="" class="our-projects-child-content__item our-projects-child-content__tablet">
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/gamrat-screens.webp" alt="" class="our-projects-child-content__item our-projects-child-content__screens">
+
+    <div class="our-projects-child-content__notes">
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">1</span>
+            <p class="our-projects-child-content__note-text">Dla czołowego producenta materiałów z WPC wykonaliśmy nowoczesną stronę www ukazującą produkty.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">2</span>
+            <p class="our-projects-child-content__note-text">Umieściliśmy na niej kalkulator, który pozwala na obliczenie ilości potrzebnej deski tarasowej.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">3</span>
+            <p class="our-projects-child-content__note-text">Oferta klienta została przedstawiona w nowoczesny, atrakcyjny sposób, powstały dedykowane infografiki.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">4</span>
+            <p class="our-projects-child-content__note-text">Zintegrowaliśmy również Gamrat WPC ze stroną Gamrat S.A.</p>
+        </div>
+        <div class="our-projects-child-content__note">
+            <span class="our-projects-child-content__note-count">5</span>
+            <p class="our-projects-child-content__note-text">Moduł blog pozwala na pozycjonowanie strony www, aby klient znalazł się wysoko w Google.</p>
+        </div>
+    </div>
+</div>
