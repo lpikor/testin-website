a52f12359c70c2b5e45a5b696337e40c975300ab
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-02 15:07:56 +0100

    Style career-benefits

--- page-kariera.php
+++ page-kariera.php
@@ -4,4 +4,6 @@
 
     get_template_part('template-parts/career-section-background');
 
+    get_template_part('template-parts/career-benefits');
+
 get_footer(); ?>
---
+++ template-parts/career-benefits.php
@@ -0,0 +1,19 @@
+<section class="career-benefits">
+    <header class="career-benefits__header">
+        <h2 class="career-benefits__title">Jest nam bardzo miło, że zainteresowała Cię praca właśnie z nami. Dołączając do naszego zespołu, możesz liczyć na:</h2>
+    </header>
+
+    <ul class="career-benefits__list">
+        <li class="career-benefits__item">rozwój umiejętności i podniesienie kwalifikacji w interesującej Cię dziedzinie,</li>
+        <li class="career-benefits__item">pogłębianie tajników tworzenia profesjonalnych stron internetowych, promocji firm w Internecie i kreacji marki,</li>
+        <li class="career-benefits__item">udział w ciekawych projektach realizowanych dla klientów z Polski i z zagranicy,</li>
+        <li class="career-benefits__item">świetną atmosferę w pracy i po pracy (stawiamy na integrację),</li>
+        <li class="career-benefits__item">każdy dzień wypełniony kreatywnymi i rozwijającymi wyzwaniami.</li>
+    </ul>
+
+    <div class="career-benefits__text">
+        <p class="career-benefits__paragraph">Jeżeli jesteś osobą nastawioną na rozwój, otwartą, potrafiącą pracować w grupie, ale równocześnie samodzielną, pomysłową i pełną pasji – koniecznie zapoznaj się z naszymi aktualnymi ofertami pracy. Być może czekamy właśnie na Ciebie!</p>
+        <p class="career-benefits__paragraph">Nawet jeżeli nie znajdziesz ogłoszenia odpowiadającego Twoim kompetencjom, ale chciałbyś nawiązać z nami współpracę – wyślij nam swoje CV i kilka słów uzasadnienia, dlaczego powinniśmy zatrudnić właśnie Ciebie.</p>
+    </div>
+
+</section>
