2f32f2d2b96ca449320b326341113c8f6f16f670
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-02 14:11:14 +0100

    Style career-section-background

--- page-kariera.php
+++ page-kariera.php
@@ -2,4 +2,6 @@
 
     get_template_part('template-parts/career-page-header');
 
+    get_template_part('template-parts/career-section-background');
+
 get_footer(); ?>
---
+++ template-parts/career-section-background.php
@@ -0,0 +1,10 @@
+<section class="section-background career-section-background">
+
+    <header class="section-background__header">
+        <h2 class="section-background__title">Dobrze trafiłeś</h2>
+    </header>
+    <div class="section-background__text">
+        <p class="section-background__paragraph">Firma Testin, agencja interaktywna z Rzeszowa, to miejsce, gdzie w życzliwej atmosferze, przy elastycznym podejściu do pracownika, rozwiniesz swoje skrzydła.</p>
+    </div>
+
+</section>
