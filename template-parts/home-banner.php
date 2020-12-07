f5372d74bb751d88e96056153c37b459b050a24c
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-07 13:51:30 +0100

    Add sub-menu styles

--- template-parts/home-banner.php
+++ template-parts/home-banner.php
@@ -1,14 +1,17 @@
 <div class="home-banner">
-    <div class="home-banner__container">
-        <h1 class="home-banner__motto">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/fastestin.webp" alt="Najszybsi w Internecie" class="home-banner__image">
-        </h1>
-        <p class="services">
-            <a href="<?php echo get_permalink(9622); ?>" class="services__item" rel="noopener noreferrer">Strony www</a>
-            <span class="services__separator">|</span>
-            <a href="<?php echo get_permalink(9614); ?>" class="services__item" rel="noopener noreferrer">Sklepy internetowe</a>
-            <span class="services__separator">|</span>
-            <a href="<?php echo get_permalink(9616); ?>" class="services__item" rel="noopener noreferrer">Marketing internetowy</a>
-        </p>
+    <div class="home-banner__box">
+        <div class="home-banner__container">
+            <h1 class="home-banner__motto">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/fastestin.webp" alt="Najszybsi w Internecie" class="home-banner__image">
+            </h1>
+            <p class="services">
+                <a href="<?php echo get_permalink(9622); ?>" class="services__item" rel="noopener noreferrer">Strony www</a>
+                <span class="services__separator">|</span>
+                <a href="<?php echo get_permalink(9614); ?>" class="services__item" rel="noopener noreferrer">Sklepy internetowe</a>
+                <span class="services__separator">|</span>
+                <a href="<?php echo get_permalink(9616); ?>" class="services__item" rel="noopener noreferrer">Marketing internetowy</a>
+            </p>
+        </div>
     </div>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/panther-jump.gif" alt="" class="home-banner__jump">
 </div>
