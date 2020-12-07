15e6406783aebc608f3fbceab9b7596a178cb109
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-07 14:14:09 +0100

    Update print-contact-us title

--- template-parts/print-contact-us.php
+++ template-parts/print-contact-us.php
@@ -1,7 +1,7 @@
 <section class="offer-child-contact-us print-contact-us">
     <div class="offer-child-contact-us__container">
         <header class="offer-child-contact-us__header">
-            <h2 class="offer-child-contact-us__title"></h2>
+            <h2 class="offer-child-contact-us__title">Pozwól się rozpoznać</h2>
             <p class="offer-child-contact-us__description">Katalogi, broszury, ulotki, kalendarze, plakaty, teczki biurowe... Czego jeszcze potrzebujesz?</p>
         </header>
         <a href="<?php echo get_permalink(9602); ?>" class="offer-child-contact-us__btn btn">Napisz do nas</a>
