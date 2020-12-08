6e3b447cbb8327cdefc273a8549888434e441233
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-08 12:44:07 +0100

    Update contact-page-header

--- template-parts/contact-page-header.php
+++ template-parts/contact-page-header.php
@@ -1,6 +1,9 @@
-<div class="page-banner-no-img contact-page-header">
-    <header class="page-banner-no-img__header">
-        <h1 class="page-banner-no-img__title">Kontakt</h1>
-        <p class="page-banner-no-img__description">Napisz, czego potrzebujesz, a my skontaktujemy się z Tobą. Strona WWW, sklep internetowy, promocja w sieci - jesteśmy do Twojej dyspozycji.</p>
+<div class="page-banner-img contact-page-header">
+    <div class="page-banner-img__img-container">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/contact.webp" alt="" class="page-banner-img__img">
+    </div>
+    <header class="page-banner-img__header">
+        <h1 class="page-banner-img__title">Kontakt</h1>
+        <p class="page-banner-img__description">Napisz, czego potrzebujesz, a my skontaktujemy się z Tobą. Strona WWW, sklep internetowy, promocja w sieci - jesteśmy do Twojej dyspozycji.</p>
     </header>
 </div>
