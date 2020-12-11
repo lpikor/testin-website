8d16f452e3fc7211b89ceccd954c0c510297a4c0
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-11 10:59:32 +0100

    Remove bugged Polish letters from buttons

--- template-parts/questions-contact.php
+++ template-parts/questions-contact.php
@@ -3,5 +3,5 @@
         <h2 class="questions-contact__title">Masz jakieś pytania?</h2>
         <p class="questions-contact__description">Jeśli potrzebujesz bardziej szczegółowych informacji o oferowanych przez nas  usługach - skontaktuj się z nami!</p>
     </header>
-    <a href="<?php echo get_permalink(9602); ?>" class="questions-contact__btn btn">Przejdź do kontaktu</a>
+    <a href="<?php echo get_permalink(9602); ?>" class="questions-contact__btn btn">Przejdz do kontaktu</a>
 </section>
