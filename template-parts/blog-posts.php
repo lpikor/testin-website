dded77a2eb27054169161967174209798825383d
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-11 15:06:03 +0100

    Add an anchor to blog-post titles

--- template-parts/blog-posts.php
+++ template-parts/blog-posts.php
@@ -6,7 +6,9 @@
 
             <article class="blog-posts__item">
                 <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="blog-posts__img">
-                <h3 class="blog-posts__title"><?php the_title(); ?></h3>
+                <a href="<?php the_permalink(); ?>">
+                    <h3 class="blog-posts__title"><?php the_title(); ?></h3>
+                </a>
                 <div class="blog-posts__description"><?php the_excerpt(); ?></div>
                 <a href="<?php the_permalink(); ?>" class="blog-posts__read-more">Czytaj dalej</a>
             </article>
