12fde74f12c2f14559e65e8c20abbb10069f9393
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-11 15:04:47 +0100

    Add the line under index title

--- template-parts/blog-page-header.php
+++ template-parts/blog-page-header.php
@@ -1,9 +1,11 @@
-<div class="page-banner-img blog-page-header">
-    <div class="page-banner-img__img-container">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/images/pen-panther.webp" alt="" class="page-banner-img__img">
+<div class="page-banner-img-container blog-page-header-container">
+    <div class="page-banner-img blog-page-header">
+        <div class="page-banner-img__img-container">
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/pen-panther.webp" alt="" class="page-banner-img__img">
+        </div>
+        <header class="page-banner-img__header">
+            <h1 class="page-banner-img__title">Blog</h1>
+            <p class="page-banner-img__description">Marketing w sieci, strony WWW, media społecznościowe - dowiedz się, co w branży piszczy i bądź na bieżąco z obowiązującymi trendami.</p>
+        </header>
     </div>
-    <header class="page-banner-img__header">
-        <h1 class="page-banner-img__title">Blog</h1>
-        <p class="page-banner-img__description">Marketing w sieci, strony WWW, media społecznościowe - dowiedz się, co w branży piszczy i bądź na bieżąco z obowiązującymi trendami.</p>
-    </header>
 </div>
