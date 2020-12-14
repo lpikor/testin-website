5893f7e70a81e90621adb505a52b317ef4f6beed
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-14 14:04:56 +0100

    Add line to kariera-page title

--- template-parts/career-page-header.php
+++ template-parts/career-page-header.php
@@ -1,9 +1,11 @@
-<div class="page-banner-img career-page-header">
-    <div class="page-banner-img__img-container">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/images/brainstorming.webp" alt="" class="page-banner-img__img">
+<div class="page-banner-img-container career-page-header-container">
+    <div class="page-banner-img career-page-header">
+        <div class="page-banner-img__img-container">
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/brainstorming.webp" alt="" class="page-banner-img__img">
+        </div>
+        <header class="page-banner-img__header">
+            <h1 class="page-banner-img__title">Dołącz do naszego zespołu!</h1>
+            <p class="page-banner-img__description">Szukasz pracy, do której każdego dnia będziesz przychodził z uśmiechem na twarzy? Gdzie podniesiesz swoje kwalifikacje, nawiążesz nowe, ciekawe znajomości i wykorzystasz potencjał, który w Tobie drzemie?</p>
+        </header>
     </div>
-    <header class="page-banner-img__header">
-        <h1 class="page-banner-img__title">Dołącz do naszego zespołu!</h1>
-        <p class="page-banner-img__description">Szukasz pracy, do której każdego dnia będziesz przychodził z uśmiechem na twarzy? Gdzie podniesiesz swoje kwalifikacje, nawiążesz nowe, ciekawe znajomości i wykorzystasz potencjał, który w Tobie drzemie?</p>
-    </header>
 </div>
