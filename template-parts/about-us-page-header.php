58465871771ba55b6d88fb7677930b6ef8eca8aa
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-14 10:21:01 +0100

    Add header underline to o-nas-page

--- template-parts/about-us-page-header.php
+++ template-parts/about-us-page-header.php
@@ -1,10 +1,12 @@
-<div class="page-banner-img about-us-page-header">
-    <div class="page-banner-img__img-container">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/images/panther-people.webp" alt="" class="page-banner-img__img">
+<div class="page-banner-img-container about-us-page-header-container">
+    <div class="page-banner-img about-us-page-header">
+        <div class="page-banner-img__img-container">
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/panther-people.webp" alt="" class="page-banner-img__img">
+        </div>
+        <header class="page-banner-img__header">
+            <h1 class="page-banner-img__title">Kim jesteśmy</h1>
+            <p class="page-banner-img__description">Agencja Interaktywna TESTIN z sukcesem działa od 2013 roku, zdobywając szerokie grono zadowolonych klientów. W naszym zespole współpracują wykwalifikowani specjaliści: programiści, graficy, pozycjonerzy i copywriterzy. Dzięki temu, możemy o klienta zadbać w pełni kompleksowo.</p>
+            <p class="page-banner-img__description">Prowadzimy pełne działania marketingowe dla wielu klientów, którym zapewniamy komfort na każdym etapie współpracy. Swoim wsparciem i doświadczeniem chcemy służyć również Tobie – Twój sukces jest również naszym sukcesem.</p>
+        </header>
     </div>
-    <header class="page-banner-img__header">
-        <h1 class="page-banner-img__title">Kim jesteśmy</h1>
-        <p class="page-banner-img__description">Agencja Interaktywna TESTIN z sukcesem działa od 2013 roku, zdobywając szerokie grono zadowolonych klientów. W naszym zespole współpracują wykwalifikowani specjaliści: programiści, graficy, pozycjonerzy i copywriterzy. Dzięki temu, możemy o klienta zadbać w pełni kompleksowo.</p>
-        <p class="page-banner-img__description">Prowadzimy pełne działania marketingowe dla wielu klientów, którym zapewniamy komfort na każdym etapie współpracy. Swoim wsparciem i doświadczeniem chcemy służyć również Tobie – Twój sukces jest również naszym sukcesem.</p>
-    </header>
 </div>
