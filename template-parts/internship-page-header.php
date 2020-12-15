268d3aa9db96f7c6578916bc7837e3aeee36d226
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-15 10:40:46 +0100

    Fix buttons

--- template-parts/internship-page-header.php
+++ template-parts/internship-page-header.php
@@ -1,6 +1,6 @@
 <div class="page-banner-img internship-page-header">
     <div class="page-banner-img__img-container">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/images/info-book-circles.png" alt="" class="page-banner-img__img">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/info-book-circles.webp" alt="" class="page-banner-img__img">
     </div>
     <header class="page-banner-img__header">
         <h1 class="page-banner-img__title">Anglista staż Rzeszów</h1>
