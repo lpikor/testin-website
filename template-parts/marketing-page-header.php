3a12c88ed05fb9e574a5436c9bbecf7a6e7a6aeb
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-15 15:21:59 +0100

    Update text alignment and textfields in marketing-page-header

--- template-parts/marketing-page-header.php
+++ template-parts/marketing-page-header.php
@@ -7,6 +7,6 @@
         <h1>
             <img src="<?php bloginfo('template_directory'); ?>/assets/images/fastestin-emarketing.webp" alt="Marketing internetowy" class="page-banner-motto__motto">
         </h1>
-        <p class="page-banner-motto__description">Nie inwestujesz w promocję w sieci? Pewnie mało kto wie o Twoim istnieniu. Blog, media społecznościowe - Internet to najlepsze narzędzie marketingu.</p>
+        <p class="page-banner-motto__description">Nie inwestujesz w promocję w sieci? Pewnie mało kto wie o Twoim istnieniu.<br />Blog, media społecznościowe - Internet to najlepsze narzędzie marketingu.</p>
     </div>
 </div>
