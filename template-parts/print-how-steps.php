d315f864aabd1698943fe93d8df10c3166d7342d
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-15 14:56:13 +0100

    Add underlines to all offer-child how-steps titles

--- template-parts/print-how-steps.php
+++ template-parts/print-how-steps.php
@@ -1,60 +1,62 @@
-<section class="offer-child-how-steps print-how-steps">
-    <header class="offer-child-how-steps__header">
-        <h2 class="offer-child-how-steps__title">Jak to robimy?</h2>
-        <p class="offer-child-how-steps__description">Zobacz, jak wygląda schemat naszej pracy drukarskiej — od pomysłu i założeń do końcowej realizacji.</p>
-    </header>
-    
-    <div class="steps">
-        <article class="steps__item">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/zoom-chart.webp" alt="" class="steps__img">
-            <div class="steps__content">
-                <div class="steps__count-container">
-                    <span class="steps__count">1</span>
+<div class="offer-child-how-steps-container print-how-steps-container">
+    <section class="offer-child-how-steps print-how-steps">
+        <header class="offer-child-how-steps__header">
+            <h2 class="offer-child-how-steps__title">Jak to robimy?</h2>
+            <p class="offer-child-how-steps__description">Zobacz, jak wygląda schemat naszej pracy drukarskiej — od pomysłu i założeń do końcowej realizacji.</p>
+        </header>
+        
+        <div class="steps">
+            <article class="steps__item">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/zoom-chart.webp" alt="" class="steps__img">
+                <div class="steps__content">
+                    <div class="steps__count-container">
+                        <span class="steps__count">1</span>
+                    </div>
+                    <h3 class="steps__title">Rozmowa z klientem</h3>
+                    <p class="steps__description">To Twoje potrzeby i oczekiwania są dla nas najważniejsze i wyznaczają kierunek prac.</p>
                 </div>
-                <h3 class="steps__title">Rozmowa z klientem</h3>
-                <p class="steps__description">To Twoje potrzeby i oczekiwania są dla nas najważniejsze i wyznaczają kierunek prac.</p>
-            </div>
-        </article>
-        <article class="steps__item">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/deal.webp" alt="" class="steps__img">
-            <div class="steps__content">
-                <div class="steps__count-container">
-                    <span class="steps__count">2</span>
+            </article>
+            <article class="steps__item">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/deal.webp" alt="" class="steps__img">
+                <div class="steps__content">
+                    <div class="steps__count-container">
+                        <span class="steps__count">2</span>
+                    </div>
+                    <h3 class="steps__title">Przygotowanie oferty</h3>
+                    <p class="steps__description">W oparciu o Twoje oczekiwania przygotowujemy propozycję działań wraz z wyceną.</p>
                 </div>
-                <h3 class="steps__title">Przygotowanie oferty</h3>
-                <p class="steps__description">W oparciu o Twoje oczekiwania przygotowujemy propozycję działań wraz z wyceną.</p>
-            </div>
-        </article>
-        <article class="steps__item">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/coffee-screen.webp" alt="" class="steps__img">
-            <div class="steps__content">
-                <div class="steps__count-container">
-                    <span class="steps__count">3</span>
+            </article>
+            <article class="steps__item">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/coffee-screen.webp" alt="" class="steps__img">
+                <div class="steps__content">
+                    <div class="steps__count-container">
+                        <span class="steps__count">3</span>
+                    </div>
+                    <h3 class="steps__title">Praca nad projektem</h3>
+                    <p class="steps__description">Jeśli jest to uwzględnione w ofercie, rozpoczynamy pracę nad projektem materiału graficznego. Jeśli Klient ma gotowy plik, przygotowujemy go do druku.</p>
                 </div>
-                <h3 class="steps__title">Praca nad projektem</h3>
-                <p class="steps__description">Jeśli jest to uwzględnione w ofercie, rozpoczynamy pracę nad projektem materiału graficznego. Jeśli Klient ma gotowy plik, przygotowujemy go do druku.</p>
-            </div>
-        </article>
-        <article class="steps__item">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/servers.webp" alt="" class="steps__img">
-            <div class="steps__content">
-                <div class="steps__count-container">
-                    <span class="steps__count">4</span>
+            </article>
+            <article class="steps__item">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/servers.webp" alt="" class="steps__img">
+                <div class="steps__content">
+                    <div class="steps__count-container">
+                        <span class="steps__count">4</span>
+                    </div>
+                    <h3 class="steps__title">Proces produkcji</h3>
+                    <p class="steps__description">Po otrzymaniu akceptacji rozpoczynamy proces produkcyjny. Pilnujemy jakości i terminu. Jesteśmy w stałym kontakcie z Klientem.</p>
                 </div>
-                <h3 class="steps__title">Proces produkcji</h3>
-                <p class="steps__description">Po otrzymaniu akceptacji rozpoczynamy proces produkcyjny. Pilnujemy jakości i terminu. Jesteśmy w stałym kontakcie z Klientem.</p>
-            </div>
-        </article>
-        <article class="steps__item">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/money-making.webp" alt="" class="steps__img">
-            <div class="steps__content">
-                <div class="steps__count-container">
-                    <span class="steps__count">5</span>
+            </article>
+            <article class="steps__item">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/money-making.webp" alt="" class="steps__img">
+                <div class="steps__content">
+                    <div class="steps__count-container">
+                        <span class="steps__count">5</span>
+                    </div>
+                    <h3 class="steps__title">Dostawa</h3>
+                    <p class="steps__description">Wydrukowane materiały trafiają pod wskazany przez Klienta adres.</p>
                 </div>
-                <h3 class="steps__title">Dostawa</h3>
-                <p class="steps__description">Wydrukowane materiały trafiają pod wskazany przez Klienta adres.</p>
-            </div>
-        </article>
+            </article>
+        </div>
     </div>
+    </section>
 </div>
-</section>
