28151fb72338a46fec60a310f51c9df6d1dfb628
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-15 13:19:49 +0100

    Add underline to websites-how-steps title

--- template-parts/websites-how-steps.php
+++ template-parts/websites-how-steps.php
@@ -1,60 +1,62 @@
-<section class="offer-child-how-steps websites-how-steps">
-    <header class="offer-child-how-steps__header">
-        <h2 class="offer-child-how-steps__title">Jak to robimy?</h2>
-        <p class="offer-child-how-steps__description">Zobacz, jak wygląda schemat naszej pracy — od pierwszego pomysłu i założeń do końcowej realizacji.</p>
-    </header>
-    
-    <div class="steps">
-        <article class="steps__item">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/zoom-chart.webp" alt="" class="steps__img">
-            <div class="steps__content">
-                <div class="steps__count-container">
-                    <span class="steps__count">1</span>
+<div class="offer-child-how-steps-container websites-how-steps-container">
+    <section class="offer-child-how-steps websites-how-steps">
+        <header class="offer-child-how-steps__header">
+            <h2 class="offer-child-how-steps__title">Jak to robimy?</h2>
+            <p class="offer-child-how-steps__description">Zobacz, jak wygląda schemat naszej pracy — od pierwszego pomysłu i założeń do końcowej realizacji.</p>
+        </header>
+        
+        <div class="steps">
+            <article class="steps__item">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/zoom-chart.webp" alt="" class="steps__img">
+                <div class="steps__content">
+                    <div class="steps__count-container">
+                        <span class="steps__count">1</span>
+                    </div>
+                    <h3 class="steps__title">Zbieramy wytyczne i założenia</h3>
+                    <p class="steps__description">Jest to jeden z najważniejszych etapów, ponieważ właśnie w tym momencie dowiadujemy się o Twoich potrzebach i oczekiwaniach.</p>
                 </div>
-                <h3 class="steps__title">Zbieramy wytyczne i założenia</h3>
-                <p class="steps__description">Jest to jeden z najważniejszych etapów, ponieważ właśnie w tym momencie dowiadujemy się o Twoich potrzebach i oczekiwaniach.</p>
-            </div>
-        </article>
-        <article class="steps__item">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/deal.webp" alt="" class="steps__img">
-            <div class="steps__content">
-                <div class="steps__count-container">
-                    <span class="steps__count">2</span>
+            </article>
+            <article class="steps__item">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/deal.webp" alt="" class="steps__img">
+                <div class="steps__content">
+                    <div class="steps__count-container">
+                        <span class="steps__count">2</span>
+                    </div>
+                    <h3 class="steps__title">Podpisujemy umowę i ustalamy harmonogram prac</h3>
+                    <p class="steps__description">Po wspólnym wypracowaniu najlepszych rozwiązań finalizujemy dotychczasowe ustalenia w formie umowy i ustalamy szczegóły dotyczące dalszej współpracy.</p>
                 </div>
-                <h3 class="steps__title">Podpisujemy umowę i ustalamy harmonogram prac</h3>
-                <p class="steps__description">Po wspólnym wypracowaniu najlepszych rozwiązań finalizujemy dotychczasowe ustalenia w formie umowy i ustalamy szczegóły dotyczące dalszej współpracy.</p>
-            </div>
-        </article>
-        <article class="steps__item">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/coffee-screen.webp" alt="" class="steps__img">
-            <div class="steps__content">
-                <div class="steps__count-container">
-                    <span class="steps__count">3</span>
+            </article>
+            <article class="steps__item">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/coffee-screen.webp" alt="" class="steps__img">
+                <div class="steps__content">
+                    <div class="steps__count-container">
+                        <span class="steps__count">3</span>
+                    </div>
+                    <h3 class="steps__title">Tworzymy projekt graficzny</h3>
+                    <p class="steps__description">Szata graficzna odgrywa bardzo ważną rolę na stronie. Oryginalny i przejrzysty motyw powinien przyciągać uwagę, a przy tym być funkcjonalny i intuicyjny.</p>
                 </div>
-                <h3 class="steps__title">Tworzymy projekt graficzny</h3>
-                <p class="steps__description">Szata graficzna odgrywa bardzo ważną rolę na stronie. Oryginalny i przejrzysty motyw powinien przyciągać uwagę, a przy tym być funkcjonalny i intuicyjny.</p>
-            </div>
-        </article>
-        <article class="steps__item">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/servers.webp" alt="" class="steps__img">
-            <div class="steps__content">
-                <div class="steps__count-container">
-                    <span class="steps__count">4</span>
+            </article>
+            <article class="steps__item">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/servers.webp" alt="" class="steps__img">
+                <div class="steps__content">
+                    <div class="steps__count-container">
+                        <span class="steps__count">4</span>
+                    </div>
+                    <h3 class="steps__title">Wdrażamy stronę online</h3>
+                    <p class="steps__description">W momencie, kiedy przygotowany projekt spełni Twoje oczekiwania, zajmiemy się jego wdrożeniem.</p>
                 </div>
-                <h3 class="steps__title">Wdrażamy stronę online</h3>
-                <p class="steps__description">W momencie, kiedy przygotowany projekt spełni Twoje oczekiwania, zajmiemy się jego wdrożeniem.</p>
-            </div>
-        </article>
-        <article class="steps__item">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/money-making.webp" alt="" class="steps__img">
-            <div class="steps__content">
-                <div class="steps__count-container">
-                    <span class="steps__count">5</span>
+            </article>
+            <article class="steps__item">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/money-making.webp" alt="" class="steps__img">
+                <div class="steps__content">
+                    <div class="steps__count-container">
+                        <span class="steps__count">5</span>
+                    </div>
+                    <h3 class="steps__title">"Testinujemy"</h3>
+                    <p class="steps__description">Na końcu sprawdzamy, czy gotowa strona działa zgodnie z założeniami i przekazujemy Ci gotowy produkt. </p>
                 </div>
-                <h3 class="steps__title">"Testinujemy"</h3>
-                <p class="steps__description">Na końcu sprawdzamy, czy gotowa strona działa zgodnie z założeniami i przekazujemy Ci gotowy produkt. </p>
-            </div>
-        </article>
+            </article>
+        </div>
     </div>
+    </section>
 </div>
-</section>
