6bdd4fc1166737169bb3b59fe503204fdb49537d
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-15 13:48:40 +0100

    Update websites-your-best description textfield

--- template-parts/websites-your-best.php
+++ template-parts/websites-your-best.php
@@ -4,7 +4,7 @@
             <img src="http://localhost/testin/wp-content/themes/testin-website/assets/images/search-screen-circles.webp" alt="" class="offer-child-your-best__img">
         </div>
         <header class="offer-child-your-best__header">
-            <h2 class="offer-child-your-best__title">Zadbaj o wizerunek swojej marki w sieci</h2>
+            <h2 class="offer-child-your-best__title">Pokaż się od najlepszej strony!</h2>
             <p class="offer-child-your-best__description">Nasi specjaliści przygotują dla Ciebie nowoczesną i funkcjonalną stronę internetową. Zadbaj o pozytywny wizerunek firmy i zwiększ zasięg swojej oferty!</p>
         </header>
     </div>
