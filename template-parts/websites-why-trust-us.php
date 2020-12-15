650e1d27758f4afece67c4c78b10dee41fa93843
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-15 13:42:46 +0100

    Add underline to websites-why-trust-us title

--- template-parts/websites-why-trust-us.php
+++ template-parts/websites-why-trust-us.php
@@ -1,7 +1,9 @@
-<section class="offer-child-why-trust-us websites-why-trust-us">
-    <header class="offer-child-why-trust-us__header">
-        <h2 class="offer-child-why-trust-us__title">Dlaczego warto nam zaufać?</h2>
-        <p class="offer-child-why-trust-us__description">Z przyjemnością rozpoczniemy z Tobą współpracę. Posiadamy doświadczenie, znamy trendy panujące na rynku i rozumiemy potrzeby naszych klientów. Strona internetowa jest wizytówką, której realizacje warto powierzyć wykonawcom godnym zaufania.</p>
-    </header>
-    <img src="<?php bloginfo('template_directory'); ?>/assets/images/cat-panther.webp" alt="" class="offer-child-why-trust-us__img">
-</section>
+<div class="offer-child-why-trust-us-container websites-why-trust-us-container">
+    <section class="offer-child-why-trust-us websites-why-trust-us">
+        <header class="offer-child-why-trust-us__header">
+            <h2 class="offer-child-why-trust-us__title">Dlaczego warto nam zaufać?</h2>
+            <p class="offer-child-why-trust-us__description">Z przyjemnością rozpoczniemy z Tobą współpracę. Posiadamy doświadczenie, znamy trendy panujące na rynku i rozumiemy potrzeby naszych klientów. Strona internetowa jest wizytówką, której realizacje warto powierzyć wykonawcom godnym zaufania.</p>
+        </header>
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/cat-panther.webp" alt="" class="offer-child-why-trust-us__img">
+    </section>
+</div>
