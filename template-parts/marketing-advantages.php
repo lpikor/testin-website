2730e60749e8af9a22a870b857748296cb20b1d8
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-15 15:49:08 +0100

    Add underline to all offer-child-advantages titles

--- template-parts/marketing-advantages.php
+++ template-parts/marketing-advantages.php
@@ -1,27 +1,29 @@
-<section class="offer-child-advantages marketing-advantages">
-    <header class="offer-child-advantages__header">
-        <h2 class="offer-child-advantages__title">Dlaczego jesteśmy tak dobrzy?</h2>
-        <p class="offer-child-advantages__description">Do każdego projektu podchodzimy w sposób indywidualny, a nasi doświadczeni specjaliści zrobią wszystko by przygotować skuteczną strategię marketingową i poprowadzić efektywne działania dla Twojej firmy.</p>
-    </header>
-
-    <div class="triple-cards">
-    <article class="triple-cards__item">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/images/dashboard.webp" alt="" class="triple-cards__img">
-        <h2 class="triple-cards__title">Po pierwsze</h2>
-        <p class="triple-cards__description triple-cards__description-strong">E-marketing jest jednym z najprężniej rozwijających się sektorów promocji. Jego celem jest m.in. przyciągnięcie uwagi potencjalnych klientów oraz zwiększenie widoczności firmy w miejscu, gdzie przebywa niemal każdy klient – Internecie.</p>
-        <p class="triple-cards__description">Do głównych zalet można zaliczyć m.in. wysoką efektywność przy stosunkowo niskich nakładach finansowych.</p>
-    </article>
-    <article class="triple-cards__item">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/images/insert-row.webp" alt="" class="triple-cards__img">
-        <h2 class="triple-cards__title">Po drugie</h2>
-        <p class="triple-cards__description triple-cards__description-strong">Marketing online pozwala na prowadzenie kreatywnych, ciekawych i spersonalizowanych działań promocyjnych.</p>
-        <p class="triple-cards__description">Dzięki możliwości wyboru grup odbiorców i odpowiednim doborze słów kluczowych trafiamy do osób potencjalnie zainteresowanych danym produktem lub usługą, przez co ograniczamy koszty niespersonalizowanej reklamy.</p>
-    </article>
-    <article class="triple-cards__item">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/images/working.webp" alt="" class="triple-cards__img">
-        <h2 class="triple-cards__title">Po trzecie</h2>
-        <p class="triple-cards__description triple-cards__description-strong">Marketing internetowy jest ważnym obszarem działań niezależnie od tego czy prowadzisz biznes online czy stacjonarnie.</p>
-        <p class="triple-cards__description">Wysoka pozycja w wynikach wyszukiwania, chwytliwe slogany, ciekawy profil w social media, przemyślane kampanie reklamowe – wszystko to ma wpływ na zainteresowanie daną firmą i wzrost jej dochodów.</p>
-    </article>
+<div class="offer-child-advantages-container marketing-advantages-container">
+    <section class="offer-child-advantages marketing-advantages">
+        <header class="offer-child-advantages__header">
+            <h2 class="offer-child-advantages__title">Dlaczego jesteśmy tak dobrzy?</h2>
+            <p class="offer-child-advantages__description">Do każdego projektu podchodzimy w sposób indywidualny, a nasi doświadczeni specjaliści zrobią wszystko by przygotować skuteczną strategię marketingową i poprowadzić efektywne działania dla Twojej firmy.</p>
+        </header>
+    
+        <div class="triple-cards">
+        <article class="triple-cards__item">
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/dashboard.webp" alt="" class="triple-cards__img">
+            <h2 class="triple-cards__title">Po pierwsze</h2>
+            <p class="triple-cards__description triple-cards__description-strong">E-marketing jest jednym z najprężniej rozwijających się sektorów promocji. Jego celem jest m.in. przyciągnięcie uwagi potencjalnych klientów oraz zwiększenie widoczności firmy w miejscu, gdzie przebywa niemal każdy klient – Internecie.</p>
+            <p class="triple-cards__description">Do głównych zalet można zaliczyć m.in. wysoką efektywność przy stosunkowo niskich nakładach finansowych.</p>
+        </article>
+        <article class="triple-cards__item">
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/insert-row.webp" alt="" class="triple-cards__img">
+            <h2 class="triple-cards__title">Po drugie</h2>
+            <p class="triple-cards__description triple-cards__description-strong">Marketing online pozwala na prowadzenie kreatywnych, ciekawych i spersonalizowanych działań promocyjnych.</p>
+            <p class="triple-cards__description">Dzięki możliwości wyboru grup odbiorców i odpowiednim doborze słów kluczowych trafiamy do osób potencjalnie zainteresowanych danym produktem lub usługą, przez co ograniczamy koszty niespersonalizowanej reklamy.</p>
+        </article>
+        <article class="triple-cards__item">
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/working.webp" alt="" class="triple-cards__img">
+            <h2 class="triple-cards__title">Po trzecie</h2>
+            <p class="triple-cards__description triple-cards__description-strong">Marketing internetowy jest ważnym obszarem działań niezależnie od tego czy prowadzisz biznes online czy stacjonarnie.</p>
+            <p class="triple-cards__description">Wysoka pozycja w wynikach wyszukiwania, chwytliwe slogany, ciekawy profil w social media, przemyślane kampanie reklamowe – wszystko to ma wpływ na zainteresowanie daną firmą i wzrost jej dochodów.</p>
+        </article>
+    </div>
+    </section>
 </div>
-</section>
