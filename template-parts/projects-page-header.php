57a0f74d8665f9096931c6e3b64edaa8ac2aff78
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-16 15:51:43 +0100

    Fix textfield and add underline to projects-page-banner title

--- template-parts/projects-page-header.php
+++ template-parts/projects-page-header.php
@@ -1,9 +1,11 @@
-<div class="page-banner-img">
-    <div class="page-banner-img__img-container">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/images/puzzle-circles.webp" alt="" class="page-banner-img__img">
+<div class="page-banner-img-container projects-page-banner-img-container">
+    <div class="page-banner-img projects-page-banner-img">
+        <div class="page-banner-img__img-container">
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/puzzle-circles.webp" alt="" class="page-banner-img__img">
+        </div>
+        <header class="page-banner-img__header">
+            <h1 class="page-banner-img__title">Nasze realizacje</h1>
+            <p class="page-banner-img__description">Różne branże, różne lokalizacje, różne grupy docelowe, ale jeden cel - widoczność firmy w sieci. Projektując i wdrażając strony i sklepy internetowe, zwracamy uwagę na każdy szczegół. Przykuwająca uwagę szata graficzna, zrozumiały tekst, przejrzystość i intuicyjność - oto kilka cech naszych realizacji.</p>
+        </header>
     </div>
-    <header class="page-banner-img__header">
-        <h1 class="page-banner-img__title">Nasze realizacje</h1>
-        <p class="page-banner-img__description">Różne branże, różne lokalizacje, różne grupy docelowe, ale jeden cel - widoczność firmy w sieci. Projektując i wdrażając strony i sklepy internetowe, zwracamy uwagę na każdy szczegół. Przykuwająca uwagę szata graficzna, zrozumiały tekst, przejrzystość i intuicyjność - oto kilka cech naszych realizacji.</p>
-    </header>
 </div>
