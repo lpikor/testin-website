c900e879096b62e06872157050a4f9346aa2b584
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-16 14:55:43 +0100

    Update service-cards-large on front-page

--- template-parts/service-cards-large.php
+++ template-parts/service-cards-large.php
@@ -3,7 +3,7 @@
         <img src="<?php bloginfo('template_directory'); ?>/assets/images/meeting.webp" alt="" class="triple-cards-large__img">
         <div class="triple-cards-large__container">
             <h2 class="triple-cards-large__title">Strony www</h2>
-            <p class="triple-cards-large__description">Tworzone przez naszą firmę witryny wyróżniają się oryginalnym designem, funkcjonalnością i mobilnością oraz intuicyjną nawigacją. Kreujemy je z myślą o atrakcyjnej prezentacji treści, zgodnie z najnowszymi trendami. Profesjonalnie przygotowane przez nas strony internetowe wzmocnią wizerunek Twojej firmy w sieci.</p>
+            <p class="triple-cards-large__description">Dbamy o to, aby każda strona www wyróżniała się unikatowym designem, była wygodna w obsłudze oraz w pełni intuicyjna. Projektowane przez nas witryny przedstawiają ofertę Klienta w atrakcyjny sposób, zarówno pod względem graficznym, jak i tekstowym. Każda z nich jest również przygotowana do pozycjonowania w Google.</p>
             <a href="<?php echo get_permalink(9622); ?>" class="triple-cards-large__btn">Czytaj dalej</a>
         </div>
     </article>
@@ -11,7 +11,7 @@
         <img src="<?php bloginfo('template_directory'); ?>/assets/images/rocket.webp" alt="" class="triple-cards-large__img">
         <div class="triple-cards-large__container">
             <h2 class="triple-cards-large__title">Sklepy internetowe</h2>
-            <p class="triple-cards-large__description">Sklepy przez nas tworzone są projektowane pod indywidualne oczekiwania klienta, ale zawsze atrakcyjne i łatwe w obsłudze. Odwiedzających je klienci bez problemu znajdą interesujący ich produkt, a także z łatwością dokonają zakupu. Ty również będziesz mógł się cieszyć jego nieskomplikowaną obsługą.</p>
+            <p class="triple-cards-large__description">Branża e-commerce cały czas się rozwija. E-sklepy zyskują na popularności, ale rośnie też konkurencja. Twój sklep musi więc się wyróżniać, aby użytkownicy zrobili akurat w nim zakupy. W Testinie tworzymy sklepy online, które atrakcyjnie się prezentują, ukazują zalety produktów oraz krok po kroku prowadzą użytkowników prosto do koszyka.</p>
             <a href="<?php echo get_permalink(9614); ?>" class="triple-cards-large__btn">Czytaj dalej</a>
         </div>
     </article>
@@ -19,8 +19,24 @@
         <img src="<?php bloginfo('template_directory'); ?>/assets/images/progress.webp" alt="" class="triple-cards-large__img">
         <div class="triple-cards-large__container">
             <h2 class="triple-cards-large__title">Marketing w sieci</h2>
-            <p class="triple-cards-large__description">Wypozycjonujemy Twoją stronę, dzięki czemu zwiększy się ilość unikalnych użytkowników oraz  nowych klientów. Wyręczymy Cię także w prowadzeniu Social Mediów oraz przygotujemy teksty, które wzmocnią jeszcze bardziej pozycję witryny i dodatkowo ją uatrakcyjnią. Zwiększymy widoczność Twojej marki w sieci.</p>
+            <p class="triple-cards-large__description">Sama strona to za mało, aby klienci znaleźli Cię w sieci. Skuteczne pozycjonowanie pomoże jej osiągnąć wysoką pozycję w wynikach wyszukiwania, a my z chęcią się tym zajmiemy! Pozycjonujemy strony www publikując na nich systematycznie nowe treści. Co więcej prowadzimy również profile w Social Media, aby kreować wizerunek marki w sieci.</p>
             <a href="<?php echo get_permalink(9616); ?>" class="triple-cards-large__btn">Czytaj dalej</a>
+        </div>
+    </article>
+    <article class="triple-cards-large__item triple-cards-large__item--branding">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/brand-name.webp" alt="" class="triple-cards-large__img">
+        <div class="triple-cards-large__container">
+            <h2 class="triple-cards-large__title">Brading</h2>
+            <p class="triple-cards-large__description">Branding to niezbędny element działań, które mają na celu zwiększenie widoczności marki na rynku. Skutecznie prowadzony, gwarantuje zdobycie zaufania i lojalności klientów, a co za tym idzie - wzrost popularności i sprzedaży produktów czy usług.</p>
+            <a href="<?php echo get_permalink(9629); ?>" class="triple-cards-large__btn">Czytaj dalej</a>
+        </div>
+    </article>
+    <article class="triple-cards-large__item triple-cards-large__item--print">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/megaphone.webp" alt="" class="triple-cards-large__img">
+        <div class="triple-cards-large__container">
+            <h2 class="triple-cards-large__title">Wydruk</h2>
+            <p class="triple-cards-large__description">Działania realizowane w ramach usługi wydruku pozwolą na dodatkową promocję usług i produktów. Katalogi, broszury, ulotki, kalendarze, plakaty, teczki biurowe - zajmiemy się projektem i realizacją. Pamiętaj - druk nigdy nie wychodzi z mody.</p>
+            <a href="<?php echo get_permalink(9627); ?>" class="triple-cards-large__btn">Czytaj dalej</a>
         </div>
     </article>
 </div>
