6785dde25a7e6eb893551e50b15bacd1bbc45eab
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-16 12:40:36 +0100

    Update branding-encouragement textfields

--- template-parts/branding-encouragement.php
+++ template-parts/branding-encouragement.php
@@ -1,8 +1,8 @@
 <section class="offer-child-encouragement branding-encouragement">
     <div class="offer-child-encouragement__container">
         <header class="offer-child-encouragement__header">
-            <h2 class="offer-child-encouragement__title">Chcesz zwiększyć zasięg Twojej oferty?</h2>
-            <p class="offer-child-encouragement__description">Nasi specjaliści poprowadzą dla Ciebie blog firmowy oraz profile w mediach społecznościowych. Dzięki temu klienci szybko znajdą Cię w sieci i poznają Twoją ofertę.</p>
+            <h2 class="offer-child-encouragement__title">Wykreuj wizerunek swojej firmy na nowo</h2>
+            <p class="offer-child-encouragement__description">Opracujemy i wdrożymy strategię rozpoznawalności Twojej marki, wskazując jej unikalne wartości oraz wzbudzając pożądanie i pozytywne emocje ze strony klientów.</p>
         </header>
         <div class="offer-child-encouragement__img-container">
             <img src="<?php bloginfo('template_directory'); ?>/assets/images/search-screen-circles-purple.webp" alt="" class="offer-child-encouragement__img">
