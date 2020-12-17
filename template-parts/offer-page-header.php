3065ac5fc04aa1933da7e521a26a514e7699ed51
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-17 13:09:49 +0100

    Add underline to offer-page-header title and fix textfields

--- template-parts/offer-page-header.php
+++ template-parts/offer-page-header.php
@@ -1,9 +1,11 @@
-<div class="page-banner-img">
-    <div class="page-banner-img__img-container">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/images/rwd.webp" alt="" class="page-banner-img__img">
+<div class="page-banner-img-container offer-page-header-container">
+    <div class="page-banner-img offer-page-header">
+        <div class="page-banner-img__img-container">
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/rwd.webp" alt="" class="page-banner-img__img">
+        </div>
+        <header class="page-banner-img__header">
+            <h1 class="page-banner-img__title">Nasza oferta</h1>
+            <p class="page-banner-img__description">Powiedz nam, jaki cel chcesz osiągnąć, a my zajmiemy się całą resztą. Przekonaj się, dlaczego warto z nami współpracować!</p>
+        </header>
     </div>
-    <header class="page-banner-img__header">
-        <h1 class="page-banner-img__title">Nasza oferta</h1>
-        <p class="page-banner-img__description">Powiedz nam, jaki cel chcesz osiągnąć, a my zajmiemy się całą resztą. Przekonaj się, dlaczego warto z nami współpracować!</p>
-    </header>
 </div>
