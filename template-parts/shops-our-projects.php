b49e55f656fbdf832b8b74d621a49a927f5b6e90
Łukasz Pikor <pikorlukasz@gmail.com>
2020-12-18 10:35:36 +0100

    Add undeline to shops-section-background title

--- template-parts/shops-our-projects.php
+++ template-parts/shops-our-projects.php
@@ -1,56 +1,58 @@
-<section class="section-background shops-section-background">
-
-    <header class="section-background__header">
-        <h2 class="section-background__title">Nasze realizacje</h2>
-        <div class="section-background__content">
-            <p class="section-background__description">Nie słowa a czyny. O wykonanych przez nas stronach moglibyśmy wiele napisać, ale warto je pokazać w całości. Zobacz, jak prezentują się dotychczasowe realizacje.</p>
-        </div>
-    </header>
+<div class="shops-section-background-container">
+    <section class="section-background shops-section-background">
     
-    <div class="our-projects">
-        <a href="<?php echo get_permalink(9635); ?>">
-            <article class="our-projects__item">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/images/allplans-showcase.webp" alt="Realizacja Allplans" class="our-projects__img">
-            </article>
-        </a>
-        <a href="<?php echo get_permalink(9637); ?>">
-            <article class="our-projects__item">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/images/pogotowie-showcase.webp" alt="Realizacja Pogotowie Warzywne" class="our-projects__img">
-            </article>
-        </a>
-        <a href="<?php echo get_permalink(9639); ?>">
-            <article class="our-projects__item">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/images/zdrojowisko-showcase.webp" alt="Realizacja Zdrojowisko" class="our-projects__img">
-            </article>
-        </a>
-        <a href="<?php echo get_permalink(9633); ?>">
-            <article class="our-projects__item">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/images/arturkucharzyk-showcase.webp" alt="Realizacja Artur Kucharzyk" class="our-projects__img">
-            </article>
-        </a>
-        <a href="<?php echo get_permalink(9631); ?>">
-            <article class="our-projects__item">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/images/polskakulturalna-showcase.webp" alt="Realizacja Polska Kulturalna" class="our-projects__img">
-            </article>
-        </a>
-        <a href="<?php echo get_permalink(9643); ?>">
-            <article class="our-projects__item">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/images/boram-showcase.webp" alt="Realizacja Boram" class="our-projects__img">
-            </article>
-        </a>
-    </div>
-
-    <section class="see-more-projects">
-        <div class="see-more-projects__decoration">
-            <img src="<?php bloginfo('template_directory'); ?>/assets/images/big-mobile.webp" class="see-more-projects__img" alt="">
+        <header class="section-background__header">
+            <h2 class="section-background__title">Nasze realizacje</h2>
+            <div class="section-background__content">
+                <p class="section-background__description">Nie słowa a czyny. O wykonanych przez nas stronach moglibyśmy wiele napisać, ale warto je pokazać w całości. Zobacz, jak prezentują się dotychczasowe realizacje.</p>
+            </div>
+        </header>
+        
+        <div class="our-projects">
+            <a href="<?php echo get_permalink(9635); ?>">
+                <article class="our-projects__item">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/images/allplans-showcase.webp" alt="Realizacja Allplans" class="our-projects__img">
+                </article>
+            </a>
+            <a href="<?php echo get_permalink(9637); ?>">
+                <article class="our-projects__item">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/images/pogotowie-showcase.webp" alt="Realizacja Pogotowie Warzywne" class="our-projects__img">
+                </article>
+            </a>
+            <a href="<?php echo get_permalink(9639); ?>">
+                <article class="our-projects__item">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/images/zdrojowisko-showcase.webp" alt="Realizacja Zdrojowisko" class="our-projects__img">
+                </article>
+            </a>
+            <a href="<?php echo get_permalink(9633); ?>">
+                <article class="our-projects__item">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/images/arturkucharzyk-showcase.webp" alt="Realizacja Artur Kucharzyk" class="our-projects__img">
+                </article>
+            </a>
+            <a href="<?php echo get_permalink(9631); ?>">
+                <article class="our-projects__item">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/images/polskakulturalna-showcase.webp" alt="Realizacja Polska Kulturalna" class="our-projects__img">
+                </article>
+            </a>
+            <a href="<?php echo get_permalink(9643); ?>">
+                <article class="our-projects__item">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/images/boram-showcase.webp" alt="Realizacja Boram" class="our-projects__img">
+                </article>
+            </a>
         </div>
-        <div class="see-more-projects__container">
-            <header class="see-more-projects__header">
-                <h3 class="see-more-projects__title">Zobacz więcej realizacji</h3>
-                <p class="see-more-projects__description">Oto niektóre z projektów, które udało nam się zrealizować dla naszych klientów. Może będą inspiracją dla Twojej strony www?</p>
-            </header>
-            <a href="<?php echo get_permalink(9596); ?>" class="see-more-projects__btn btn">Realizacje</a>
-        </div>
+    
+        <section class="see-more-projects">
+            <div class="see-more-projects__decoration">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/big-mobile.webp" class="see-more-projects__img" alt="">
+            </div>
+            <div class="see-more-projects__container">
+                <header class="see-more-projects__header">
+                    <h3 class="see-more-projects__title">Zobacz więcej realizacji</h3>
+                    <p class="see-more-projects__description">Oto niektóre z projektów, które udało nam się zrealizować dla naszych klientów. Może będą inspiracją dla Twojej strony www?</p>
+                </header>
+                <a href="<?php echo get_permalink(9596); ?>" class="see-more-projects__btn btn">Realizacje</a>
+            </div>
+        </section>
+    
     </section>
-
-</section>
+</div>
